da9ac528fd3520bc6b25e91f92aacab3dffb08a7
Quobb <kueprotocol11@gmail.com>
2023-08-02 04:37:40 +0000

    dd

--- hospital/app/src/main/java/com/example/hospital/login.java
+++ hospital/app/src/main/java/com/example/hospital/login.java
@@ -2,13 +2,65 @@ package com.example.hospital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
-
+    EditText user, pass;
+    Button log ,sign;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user = findViewById(R.id.txtuser);
+        pass = findViewById(R.id.txtpass);
+        log = findViewById(R.id.btnlog);
+        sign = findViewById(R.id.btnsign);
+        log.setOnClickListener(v -> {
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            if(username.length() == 0 || password.length()==0){
+                Toast.makeText(getApplicationContext(),"Enter detail",Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(getApplicationContext(),"log successful",Toast.LENGTH_LONG).show();
+            }
+
+            Toast.makeText(getApplicationContext(),"log successful",Toast.LENGTH_LONG).show();
+            Intent dd = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(dd);
+        });
+        TextWatcher kk = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+                log.setEnabled(!(username.isEmpty() || password.isEmpty()));
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        user.addTextChangedListener(kk);
+        pass.addTextChangedListener(kk);
     }
+  public void regis(){
+        Intent hh = new Intent(getApplicationContext(), register.class);
+        startActivity(hh);
+  }
 }
--- quobii/app/src/main/java/com/example/quobii/login.java
+++ quobii/app/src/main/java/com/example/quobii/login.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.icu.text.ConstrainedFieldPosition;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class login extends AppCompatActivity {
   EditText txtuser,txtuserpass;
   Button btnenter,btncan;
+  View newmwin;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,7 @@ public class login extends AppCompatActivity {
         txtuserpass = findViewById(R.id.txtpass);
         btnenter = findViewById(R.id.btnlog);
         btncan = findViewById(R.id.btncancel);
+        newmwin = findViewById(R.id.main);
         btncan.setOnClickListener(v -> {
             txtuser.setText("");
             txtuserpass.setText("");
@@ -34,6 +41,33 @@ public class login extends AppCompatActivity {
                 Toast.makeText(login.this,"invaid username or password" ,Toast.LENGTH_SHORT).show();
             }
         });
+        btnenter.setEnabled(false);
+
+        // Set up a common text change listener for both EditText fields
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Enable the login button if both username and password are not empty
+                boolean isUsernameEmpty = txtuser.getText().toString().trim().isEmpty();
+                boolean isPasswordEmpty = txtuserpass.getText().toString().trim().isEmpty();
+                btnenter.setEnabled(!(isUsernameEmpty || isPasswordEmpty));
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        txtuser.addTextChangedListener(textWatcher);
+        txtuserpass.addTextChangedListener(textWatcher);
+
+
     }
     public void logback(View mm){
         Intent kk = new Intent(getApplicationContext(),mobile_computing.class);
